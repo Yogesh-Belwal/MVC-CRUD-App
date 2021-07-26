d3c2d8374d05e29105f7ed5779a3fbaa50c8e514
Yogesh Belwal <65054623+Yogesh-Belwal@users.noreply.github.com>
2021-07-26 17:48:31 +0530

    Files upload

---
+++ CRUD/Controllers/EmployeesController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUD;
+
+namespace CRUD.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private QuestionEntities db = new QuestionEntities();
+
+        // GET: Employees
+        public ActionResult Index()
+        {
+            List<Employee> employees = db.Employees.Include(e => e.Department).Include(e => e.Salary).ToList();
+
+            var asd = from e in employees
+                     orderby e.Salary.SalaryAmount descending, e.Name
+                     select e;
+
+            return View(asd.ToList());
+        }
+
+        // GET: Employees/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: Employees/Create
+        public ActionResult Create()
+        {
+            ViewBag.DeptID = new SelectList(db.Departments, "DeptID", "DptName");
+            ViewBag.EmployeeId = new SelectList(db.Salaries, "EmployeeId", "EmployeeId");
+            return View();
+        }
+
+        // POST: Employees/Create
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmployeeId,DeptID,Name,DOJ,Mobile,Email,Address")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Employees.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.DeptID = new SelectList(db.Departments, "DeptID", "DptName", employee.DeptID);
+            ViewBag.EmployeeId = new SelectList(db.Salaries, "EmployeeId", "EmployeeId", employee.EmployeeId);
+            return View(employee);
+        }
+
+        // GET: Employees/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.DeptID = new SelectList(db.Departments, "DeptID", "DptName", employee.DeptID);
+            ViewBag.EmployeeId = new SelectList(db.Salaries, "EmployeeId", "EmployeeId", employee.EmployeeId);
+            return View(employee);
+        }
+
+        // POST: Employees/Edit/5
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmployeeId,DeptID,Name,DOJ,Mobile,Email,Address")] Employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.DeptID = new SelectList(db.Departments, "DeptID", "DptName", employee.DeptID);
+            ViewBag.EmployeeId = new SelectList(db.Salaries, "EmployeeId", "EmployeeId", employee.EmployeeId);
+            return View(employee);
+        }
+
+        // GET: Employees/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Employee employee = db.Employees.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: Employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Employee employee = db.Employees.Find(id);
+            db.Employees.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ CRUD/Controllers/SalariesController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUD;
+
+namespace CRUD.Controllers
+{
+    public class SalariesController : Controller
+    {
+        private QuestionEntities db = new QuestionEntities();
+
+        // GET: Salaries
+        public ActionResult Index()
+        {
+            var salaries = db.Salaries.Include(s => s.Employee);
+            return View(salaries.ToList());
+        }
+
+        // GET: Salaries/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Salary salary = db.Salaries.Find(id);
+            if (salary == null)
+            {
+                return HttpNotFound();
+            }
+            return View(salary);
+        }
+
+        // GET: Salaries/Create
+        public ActionResult Create()
+        {
+            ViewBag.EmployeeId = new SelectList(db.Employees, "EmployeeId", "Name");
+            return View();
+        }
+
+        // POST: Salaries/Create
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmployeeId,SalaryAmount")] Salary salary)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Salaries.Add(salary);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.EmployeeId = new SelectList(db.Employees, "EmployeeId", "Name", salary.EmployeeId);
+            return View(salary);
+        }
+
+        // GET: Salaries/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Salary salary = db.Salaries.Find(id);
+            if (salary == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.EmployeeId = new SelectList(db.Employees, "EmployeeId", "Name", salary.EmployeeId);
+            return View(salary);
+        }
+
+        // POST: Salaries/Edit/5
+   
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmployeeId,SalaryAmount")] Salary salary)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(salary).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.EmployeeId = new SelectList(db.Employees, "EmployeeId", "Name", salary.EmployeeId);
+            return View(salary);
+        }
+
+        // GET: Salaries/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Salary salary = db.Salaries.Find(id);
+            if (salary == null)
+            {
+                return HttpNotFound();
+            }
+            return View(salary);
+        }
+
+        // POST: Salaries/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Salary salary = db.Salaries.Find(id);
+            db.Salaries.Remove(salary);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
